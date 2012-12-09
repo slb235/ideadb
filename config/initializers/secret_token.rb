bb4b86e9beb888f949ed1b54dcb8d7e1113d82c8
Jonathan <rauprich.jonathan@googlemail.com>
2012-12-09 18:17:54 +0100

    initial import (basic rails structure)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideadb::Application.config.secret_token = '4fad95a26e43ab73f24830a0e5a81627d42bebc5fc05e7a5a20f89a3802943c294bd463f1968f5e868cd88ff71093a50c83d0752025b1910ac7f0eb5128f2529'
