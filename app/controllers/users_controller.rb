274333925b1dcc37b9072417169672b324245143
Jonathan Rauprich <rauprich.jonathan@googlemail.com>
2013-03-11 12:30:14 +0100

    minor redirection fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
-      redirect_to @user
+      redirect_to '/dashboard'
     else
       render 'edit'
     end
