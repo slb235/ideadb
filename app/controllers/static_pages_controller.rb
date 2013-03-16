1c42891aeebde9de56c9fedbdf556718b9d13631
Jonathan Rauprich <rauprich.jonathan@googlemail.com>
2013-03-16 22:49:32 +0100

    redirect to dashboard or signin from homepage. closes #9

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   before_filter :signed_in_user, :only => :dash
 
   def home
+    redirect_to signin_path unless signed_in?
+    redirect_to dashboard_path if signed_in?
   end
 
   def help
